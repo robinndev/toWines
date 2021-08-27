3993db60be855b7c354ba4d617eb966b7e708703
robinndev <robson_dev@hotmail.com>
2021-08-27 14:52:45 -0300

    adicionando Header

---
+++ towines/src/components/Header/Index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import styled from "./styled.module.scss"
+import logo from "../../assets/logo.png"
+
+export const Header = () => {
+    return (
+        <div className={styled.headerContainer}>
+            <img className={styled.logoImage} src={logo}/>
+
+            <div className={styled.navbarContainer}>
+                <a className={styled.tagA} href="/"><p>HOME</p></a>
+                <a className={styled.tagA} href="products"><p>PRODUCTS</p></a>
+                <a className={styled.tagA} href="faqs"><p>FAQS</p></a>
+                <a className={styled.tagA} href="about"><p>ABOUT</p></a>
+            </div>
+        </div>
+    )
+}
