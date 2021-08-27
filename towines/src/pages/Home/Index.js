6d308d07cf9b178104fc33d2bcf389332d0d73f6
robinndev <robson_dev@hotmail.com>
2021-08-27 14:58:26 -0300

    adicionando funcionalidades ao Header

--- towines/src/pages/Home/Index.js
+++ towines/src/pages/Home/Index.js
@@ -12,7 +12,7 @@ return(
             <div className={styled.textContainer}>
                 <p className={styled.textPrincipal}>GREAT WINE</p>
                 <p className={styled.subtitleText}>he standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum"</p>
-                <button className={styled.buttonProducts}>Products</button>
+                <a className={styled.linkButtonProduct} href="/products"><button className={styled.buttonProducts}>Products</button></a>
                 
             </div>
         </div>
